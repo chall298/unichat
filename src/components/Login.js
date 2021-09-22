404d41888c0e6e739d7151a2b84ffc6121818816
chall298 <chall361@gmail.com>
2021-09-21 22:14:12 -0400

    added google auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
+import 'firebase/app'
+
+import { auth } from '../firebase'
+import firebase from 'firebase/app'
 
 const Login = () => {
     return (
@@ -9,6 +13,7 @@ const Login = () => {
 
                 <div
                     className="login-button google"
+                    onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
                 >
                     <GoogleOutlined /> Sign In with Google
                 </div>
@@ -17,6 +22,7 @@ const Login = () => {
 
                 <div
                     className="login-button facebook"
+                    onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
                 >
                     <FacebookOutlined /> Sign In with Facebook
                 </div>
